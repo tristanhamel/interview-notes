b2b53349b0737b080b3e88128d7b9ff2c7f1bab0
tristanhamel <tristan.hamel@kielikone.fi>
2017-06-08 21:51:30 +0300

    table bg according to score

--- src/components/group-view/QuestionnairesTable.jsx
+++ src/components/group-view/QuestionnairesTable.jsx
@@ -5,6 +5,13 @@ import { PGroupReselect } from '../../proptypes';
 import { QuestionnaireTableCell } from './QuestionnaireTableCell';
 
 export const QuestionnairesTable = (props) => {
+  const color = (score) => {
+    // https://stackoverflow.com/questions/17525215/calculate-color-values-from-green-to-red
+    // as the function expects a value between 0 and 1, and red = 0° and green = 120°
+    // we convert the input to the appropriate hue value
+    return score != null ? `hsl(${score * 120}, 80%, 80%)` : 'none';
+  };
+
   return (
     <div>
       <table className="table">
@@ -39,7 +46,8 @@ export const QuestionnairesTable = (props) => {
                     const response = q.responses.find(r => r.questionId === question.id);
 
                     return (
-                      <td key={k}>
+                      <td key={k}
+                          style={response ? {background: color(response.score)} : {}}>
                         {response && typeof response.value != 'undefined' &&
                         <QuestionnaireTableCell question={question}
                                                 response={response}/>
