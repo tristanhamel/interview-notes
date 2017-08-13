dfe17dd71105517403303ca71b5e519d2b06f6e8
tristanhamel <tristan.hamel@kielikone.fi>
2017-08-13 14:05:31 +0300

    persist state

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ import 'whatwg-fetch';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import persistState from 'redux-localstorage';
 import  thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { enableBatching } from 'redux-batched-actions';
 
 import { rootReducer } from './reducers/reducers';
 
-import initialState from './dummy-state.json';
-// import { initialState } from './reducers/initialState';
+//import initialState from './dummy-state.json';
+ import { initialState } from './reducers/initialState';
 
 import { Menu } from './components/Menu';
 import { Main } from './components/Main';
@@ -30,11 +31,15 @@ const App = () => (
   </div>
 );
 
+const excludedPaths = ['ui'];
+const persistedPaths = Object.keys(initialState).filter( k => !excludedPaths.includes(k));
+
 const store = createStore(
   enableBatching(rootReducer),
   initialState,
   composeWithDevTools(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk),
+    persistState(persistedPaths /*, config*/)
   )
 );
 
